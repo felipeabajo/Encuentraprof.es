d7f25f54e2699cec41761e0d2bd49f4a3306153c
felipeabajo <felipeabajo@gmail.com>
2023-12-10 15:31:19 +0100

    Improve system for gathering Provinces and Specialities. Requirements: provinces and specialities available for editing Personal Information and also for searching teachers. Solution: (simplest I can figure out) put that info into separate classes, using public static list for gathering elements.

---
+++ BlazorServerSQLite/Data/Speciality.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorServerSQLite.Data
+{
+    public class Speciality
+    {
+        public string Name { get; set; }
+        public int Code { get; set; }
+
+        public static List<Speciality> Specialities = new List<Speciality>
+        {
+        new Speciality (){Name = "French", Code = 01},
+        new Speciality (){Name = "English", Code = 02},
+        new Speciality (){Name = "Spanish", Code = 03}
+        };
+    }
+}
